89e57db75b79570f07aa1310fe8412defbbcb020
Dave Harms <daveharmswebdev@gmail.com>
2022-07-03 14:01:04 -0500

    initial commit

---
+++ src/main/java/com/eitheror/eitherorresource/EitherOrResourceApplication.java
@@ -0,0 +1,13 @@
+package com.eitheror.eitherorresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EitherOrResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EitherOrResourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eitheror/eitherorresource/EitherOrResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eitheror.eitherorresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EitherOrResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
